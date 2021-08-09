bc09b861c0716b8b0c27f94dfb9b84284a361f64
F4isalSH <f4isalcode@gmail.com>
2021-08-09 16:03:29 +0300

    Successfully fetch and output weather info in console

--- weatherData.js
+++ weatherData.js
@@ -4,19 +4,19 @@ const apiKey = 'X6emy93Gz3jfFPnvznjlVzKFcl9KD7hh'
 
 // Function for getting the city key
 const getCity = async (city) =>{
-    let baseUrl = 'http://dataservice.accuweather.com/locations/v1/cities/search'
-    let query   = `?apikey=${apiKey}&q=${city}`
-    let response = await fetch(baseUrl + query);
-    let data = await response.json();
+    const baseUrl = 'http://dataservice.accuweather.com/locations/v1/cities/search'
+    const query   = `?apikey=${apiKey}&q=${city}`
+    const response = await fetch(baseUrl + query);
+    const data = await response.json();
     return data[0].Key
 }
 
 
 // Function using the city key to get the weather
 const getWeather = async (locationKey) =>{
-    let baseurl = `http://dataservice.accuweather.com/currentconditions/v1/${locationKey}?apikey=${apiKey}`
-    let response = await fetch(baseurl);
-    let data = await response.json();
+    const baseurl = `http://dataservice.accuweather.com/currentconditions/v1/${locationKey}?apikey=${apiKey}`
+    const response = await fetch(baseurl);
+    const data = await response.json();
     return data[0];
 }
 
