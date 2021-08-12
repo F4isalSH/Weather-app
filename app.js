b00cce299cc3e84d9fd0451054a63e0def37d843
F4isalSH <f4isalcode@gmail.com>
2021-08-12 16:03:29 +0300

    Added design template for the weather output. Removed app.js functions (will be worked on later)

--- app.js
+++ app.js
@@ -1,15 +1,2 @@
-const search = document.querySelector('.search');
-
-
-
-search.addEventListener('submit', e=>{
-    e.preventDefault();
-    const location = e.target.input.value;
-    getCity(location)
-    .then(data => getWeather(data))
-    .then(data => console.log(data))
-    .catch(err => console.log(err))
-    search.reset()
-})
 
 
