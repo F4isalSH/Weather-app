c5f37cc34b9d3149542c61a7a41c4adc5e992509
F4isalSH <f4isalcode@gmail.com>
2021-08-14 01:40:23 +0300

    Changed the baseurl for accuweather to https due to fix the error

--- weatherData.js
+++ weatherData.js
@@ -4,7 +4,7 @@ const apiKey = 'PDvA14HViuM7ibxIGrxDBHm2ww3JtZF2'
 
 // Function for getting the city key
 const getCity = async (city) =>{
-    const baseUrl = 'http://dataservice.accuweather.com/locations/v1/cities/search'
+    const baseUrl = 'https://dataservice.accuweather.com/locations/v1/cities/search'
     const query   = `?apikey=${apiKey}&q=${city}`
     const response = await fetch(baseUrl + query);
     const data = await response.json();
@@ -14,7 +14,7 @@ const getCity = async (city) =>{
 
 // Function using the city key to get the weather
 const getWeather = async (locationKey) =>{
-    const baseurl = `http://dataservice.accuweather.com/currentconditions/v1/${locationKey}?apikey=${apiKey}`
+    const baseurl = `https://dataservice.accuweather.com/currentconditions/v1/${locationKey}?apikey=${apiKey}`
     const response = await fetch(baseurl);
     const data = await response.json();
     return data[0];
