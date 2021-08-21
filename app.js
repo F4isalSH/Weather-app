db4fdd1847b8acfe501d096138f1e95cec0ff521
F4isalSH <f4isalcode@gmail.com>
2021-08-21 21:19:06 +0300

    Added local storage

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const locationForm = document.querySelector('.locationForm')
-const weatherList = document.querySelector('.weather-list')
+const weatherCard = document.querySelector('.container')
+
 
 const allDetails = async (location) =>{
     const cityDets = await getCity(location)
@@ -10,37 +11,55 @@ const allDetails = async (location) =>{
     }
 }
 
+const updateCard = (location) =>{
+        localStorage.setItem('location', location)
+        allDetails(location)
+        .then(data => addLocation(data))
+        .catch(err => console.log(err))
+}
+
+
 const addLocation = (data) =>{
     const temperature = data.weatherDets.Temperature.Metric.Value;
     const location = data.cityDets.EnglishName;
     if(data.weatherDets.IsDayTime){
         let html =
-        `<li class="m-2 weather-item">
-        <div class="weather-header display-5 bg-light text-center "><span>${location}</span></div>
+        `
+        <div class = "weather-card">
+        <div class="weather-header display-5 bg-light text-center"><span>${location}</span></div>
         <img src="img/day.svg" alt="">
         <div class="temperature display-3"><span>${temperature}°C</span></div>
-        </li>`
-        weatherList.innerHTML += html;
+        </div>
+        `
+        weatherCard.innerHTML += html;
     }else{
 
-        let html = `<li class="m-2 weather-item">
+        let html = `
+        <div class = "weather-card">
         <div class="weather-header display-5 text-center text-light"><span>${location}</span></div>
         <img src="img/night.svg" alt="">
-        <div class="temperature display-3 text-light"><span>${temperature}°C</span></div>
-    </li>`  
-        weatherList.innerHTML += html;
+        <div class="temperature display-3 text-light"><span>${temperature}°C</span></div>>
+        </div>`  
+        weatherCard.innerHTML += html;
     }
 }
 
 
 locationForm.addEventListener(('submit'), e =>{
     e.preventDefault()
-    allDetails(e.target.location.value)
-    .then(data => addLocation(data))
-    .catch(err => console.log(err))
+    if(localStorage.getItem('location')){
+        let card = document.querySelector('.weather-card')
+        card.parentNode.removeChild(card)
+        updateCard(e.target.location.value)
+    }else{
+        updateCard(e.target.location.value)
+    }
 
     locationForm.reset()
 })
 
+if(localStorage.getItem('location')){
+    updateCard(localStorage.getItem('location'))
+}
 
 
